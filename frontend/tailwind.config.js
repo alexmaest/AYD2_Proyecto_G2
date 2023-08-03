7d0772297e2c8a5eee531bcf559f592906d36e6f
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-08-03 16:53:53 -0600

    remove nextui configuracion

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,12 @@
-const { nextui } = require('@nextui-org/react')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {}
   },
-  darkMode: 'class',
-  plugins: [nextui({
-    themes: {
-      dark: {
-        colors: {
-          'retro-white': '#F3EFE0',
-          'retro-black': '#222222',
-          'retro-green': '#1B5045',
-          'retro-orange': '#F3AA60',
-          'retro-blue': '#1D5B79'
-        }
-      }
-    }
-  })]
+  plugins: []
 }
