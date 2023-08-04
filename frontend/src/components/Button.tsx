432001080dadb91a22f656fcd4b965d31babd2fa
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-08-04 16:38:02 -0600

    feature: create login view

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
 interface ButtonProps {
   type: 'primary' | 'secondary'
+  width?: string
   children: React.ReactNode
 }
 
@@ -8,10 +9,10 @@ const buttonStyles = {
   secondary: 'bg-retro-orange hover:brightness-110 hover:scale-105'
 }
 
-function Button ({ type, children }: ButtonProps) {
+function Button ({ type, width, children }: ButtonProps) {
   return (
     <button
-      className={`group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out ${buttonStyles[type]}`}
+      className={`group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out ${buttonStyles[type]} ${width !== undefined ? width : ''}`}
     >
       {children}
     </button>
