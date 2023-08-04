4151a7ea46dd6e15820a67022c8e1b82e2a141f5
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-08-03 21:34:34 -0600

    add navbar to admin page

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -6,8 +6,14 @@ export default function AdminLayout ({
 }) {
   return (
     <main className='h-screen'>
-      <nav />
-      {children}
+      <nav className='fixed w-full top-0 p-4'>
+        <ul>
+          <li>Retro Music</li>
+        </ul>
+      </nav>
+      <section className='mt-14'>
+        {children}
+      </section>
     </main>
   )
 }
--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function AdminPage () {
   return (
-    <section>
+    <div>
       <h1>Admin Page</h1>
-    </section>
+    </div>
   )
 }
