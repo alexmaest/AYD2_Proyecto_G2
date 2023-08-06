7a75194e15a0e1787528795eb1c90eef72e7c0c7
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-08-05 19:29:49 -0600

    add basic artist page

---
+++ frontend/src/app/artist/layout.tsx
@@ -0,0 +1,23 @@
+import Brand from '@/components/Brand'
+
+export default function ArtistLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main>
+      <nav className='fixed w-full top-0 px-20 py-4 bg-retro-black'>
+        <ul className='flex items-center justify-between'>
+          <li className='font-semibold'>
+            <Brand className='h-[36px] w-[136.8px]' color='#F3EFE0' />
+          </li>
+          <div className='flex space-x-4'>
+            <li className='text-white'>Inicio</li>
+          </div>
+        </ul>
+      </nav>
+      {children}
+    </main>
+  )
+}
