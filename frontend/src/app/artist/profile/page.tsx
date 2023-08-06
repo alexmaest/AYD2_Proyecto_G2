15daec75f0732024e914315b910e1709fd1993b4
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-08-05 21:32:41 -0600

    add post malone banner

--- frontend/src/app/artist/profile/page.tsx
+++ frontend/src/app/artist/profile/page.tsx
@@ -1,7 +1,21 @@
+import { BsFillPatchCheckFill } from 'react-icons/bs'
+
 export default function ProfilePage () {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className=''>
+      <section className='m-6 mt-20'>
+        <article className='relative -z-10 shadow-2xl shadow-red-500'>
+          <div className='flex absolute bottom-1/2 ml-6 space-x-3'>
+            <BsFillPatchCheckFill className='w-6 h-6 text-retro-orange-500' />
+            <p className='text-white'>Artista verificado</p>
+          </div>
+          <h1 className='absolute bottom-1/3 ml-6 font-extrabold text-6xl text-white'>Post Malone</h1>
+          <img
+            className='block w-full h-96 object-cover rounded-lg'
+            src='https://www.udiscovermusic.com/wp-content/uploads/2022/06/Post-Malone-Adam-De-Gross-copy.jpg' alt='Artist banner'
+          />
+        </article>
+      </section>
     </div>
   )
 }
