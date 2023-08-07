84283866810ee4bcf4e1299453e10cbc6c363b93
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-08-06 18:31:25 -0600

    chore: sign the user in

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,12 +2,17 @@
 import Button from '@/components/Button'
 import Input from '@/components/Input'
 import Link from 'next/link'
-import { useMemo, useState } from 'react'
+import { useSession, signIn } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import { useEffect, useMemo, useState } from 'react'
 import { TbEye, TbEyeOff } from 'react-icons/tb'
 
 function Login () {
+  const { data: session } = useSession()
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const [showPassword, setShowPassword] = useState(false)
 
   const validationStateEmail = useMemo(() => {
@@ -24,11 +29,62 @@ function Login () {
     return (validatePassword(password)) ? 'valid' : 'invalid'
   }, [password])
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    let allValid = true
+
+    if (validationStateEmail === 'invalid') {
+      allValid = false
+    }
+
+    if (validationStatePassword === 'invalid') {
+      allValid = false
+    }
+
+    if (!allValid) {
+      return
+    }
+
+    try {
+      const res = await signIn('credentials', {
+        email,
+        password,
+        redirect: false
+      })
+      if (res?.status === 401) {
+        setError('El usuario aún no está habilitado o no existe!')
+      }
+    } catch (error) {
+      setError('Correo o contraseña incorrectos')
+    }
+  }
+  useEffect(() => {
+    setError('')
+  }, [email, password])
+
+  useEffect(() => {
+    if (session?.user != null) {
+      const role = session?.user.role
+      if (role === 0) {
+        redirect('/admin')
+      } else {
+        redirect('/artist')
+      }
+    }
+  }, [session])
+
   return (
 
     <main className='flex items-center justify-center my-16'>
-      <form action='post' className='flex p-16 gap-12 flex-col items-center bg-[#1D1D1D]'>
+      <form
+        action='post'
+        className='flex p-16 gap-12 flex-col items-center bg-[#1D1D1D]'
+        onSubmit={handleSubmit}
+      >
         <h2 className='text-4xl font-bold text-retro-white'>Login to RetroMusic</h2>
+        {error !== '' && (
+          <p className='border-2 border-red-500 text-red-500 text-lg text-center italic p-2'>{error}</p>
+        )}
         <section className='flex flex-col items-start gap-6'>
           <Input
             value={email}
