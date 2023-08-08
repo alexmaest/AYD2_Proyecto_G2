fc368f3c80412c37d70050753ddeaf5613372dd9
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-08-08 16:27:41 -0600

    add fetch to send banner

--- frontend/src/app/artist/profile/banner/page.tsx
+++ frontend/src/app/artist/profile/banner/page.tsx
@@ -1,9 +1,15 @@
 'use client'
+import { apiUrls, baseUrl } from '@/constants/urls'
 import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import Alert from '@/components/Alert'
 
 export default function BannerPage () {
+  const { data } = useSession()
   const [file, setFile] = useState<File | null>(null)
   const [base64Image, setBase64Image] = useState<string | null>(null)
+  const [error, setError] = useState('')
+  const [isAlertOpen, setIsAlertOpen] = useState(false)
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
@@ -25,10 +31,40 @@ export default function BannerPage () {
     console.log(base64Image)
   }
 
-  console.log(base64Image)
+  const handleUpload = async () => {
+    if (file != null) {
+      /*
+      Enviar la imagen en base64 al servidor
+      */
+      try {
+        const response = await fetch(`${baseUrl}${apiUrls.artist.uploadBanner}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            userId: data?.user.id,
+            image: base64Image
+          })
+        })
+
+        if (!response.ok) {
+          setError('Error al subir la imagen')
+          setIsAlertOpen(true)
+          throw new Error('Error al subir la imagen')
+        }
+      } catch (error) {
+        setError('Algo salió mal al subir la imagen')
+        setIsAlertOpen(true)
+      }
+    }
+  }
 
   return (
     <section className='mt-20 mx-6 flex flex-col items-center'>
+      <Alert type='danger' className='w-[450px]' isOpen={isAlertOpen} onClick={() => setIsAlertOpen(false)}>
+        <p>{error}</p>
+      </Alert>
       <div className='flex items-center justify-center w-full'>
         <label htmlFor='dropzone-file' className='flex flex-col items-center justify-center w-1/2 h-44 border-2 border-dashed rounded-lg cursor-pointer bg-retro-black-800 border-retro-black-600 hover:border-retro-black-500 hover:bg-retro-black-600'>
           <div className='flex flex-col items-center justify-center pt-5 pb-6'>
@@ -41,7 +77,10 @@ export default function BannerPage () {
           <input id='dropzone-file' type='file' className='hidden' onChange={handleFileChange} />
         </label>
         <div className='flex items-center justify-center flex-1'>
-          <button className='rounded w-2/4 h-10 bg-retro-orange-400 shadow-lg shadow-orange-300/50'>
+          <button
+            className='rounded w-2/4 h-10 bg-retro-orange-400 shadow-lg shadow-orange-300/50'
+            onClick={handleUpload}
+          >
             Subir
           </button>
         </div>
