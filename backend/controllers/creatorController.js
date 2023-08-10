5887684f6d01ac9fc1af4f29386eda0d8323b31a
Fernando Mauricio Gómez Santos <70865380+Fernando-MGS@users.noreply.github.com>
2023-08-09 22:15:12 -0600

    Actualización de respuesta del post del banner

--- backend/controllers/creatorController.js
+++ backend/controllers/creatorController.js
@@ -12,8 +12,11 @@ class CreatorController { //FG
                 res.status(401).send('Error')
             }else{
                 const bannerCreator = await creatorModel.updateBanner(banner,req.body.userId)
-                console.log("bannerCreator")
-                console.log(bannerCreator   )
+                if (bannerCreator === null){
+                    res.status(401).send('Error')
+                }else{
+                    res.status(200).send(true)
+                }
             }
         } catch(err){
             console.error(err);
--- backend/repositories/creatorRepository.js
+++ backend/repositories/creatorRepository.js
@@ -2,14 +2,12 @@ const db = require('../database');
 
 class creatorRepository {
     save(creator){
-        const urlBanner = 'https://i.ytimg.com/vi/k3TZZVEEKao/hqdefault.jpg';
         return new Promise((resolve,reject) => {
             const query = `
-                INSERT INTO creador_contenido (usuario_id,banner) VALUES (?,?);
+                INSERT INTO creador_contenido (usuario_id) VALUES (?);
                 `;
             const values = [
-                creator.id_usuario,
-                urlBanner
+                creator.userId,
             ]
 
             db.connection.query(query, values, (err, result) => {
@@ -29,7 +27,7 @@ class creatorRepository {
           const query = 'UPDATE creador_contenido SET banner = ? WHERE usuario_id = ?';
           db.connection.query(query, [url, creator], (err, result) => {
             if (err) {
-              reject(err);
+              reject(null);
             } else {
               resolve(result.affectedRows > 0);
             }
