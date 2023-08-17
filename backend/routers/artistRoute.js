3d3eb938fc7ada11afe3e79aa1d8c4b82ca701e4
Alexis Estrada <alexis1estrada@hotmail.com>
2023-08-17 17:37:55 -0600

    Add upload song
    -Added upload song to bucket and storage db endpoint
    -Added all artist songs get endpoint

--- backend/routers/artistRoute.js
+++ backend/routers/artistRoute.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const upload = multer();
 
 const artistController = require('../controllers/artistController');
 const songController = require('../controllers/songController');
 
 router.get('/banner/:userId', artistController.getBanner);
+router.get('/songs/:userId', songController.getSongs);
 
 router.post('/banner', artistController.updateBanner);
 router.post('/ProfileConfig', artistController.updateProfile);//JA
-router.post('/uploadSong', upload.single('song'), songController.createSong);
+router.post('/uploadSong', songController.createSong);
 
 module.exports = router;
