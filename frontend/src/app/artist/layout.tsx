8485a21ef94f7aa9b69fc687efb39eee19ba9d27
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-08-31 15:49:32 -0600

    chore: created and implemented dropdown button

--- frontend/src/app/artist/layout.tsx
+++ frontend/src/app/artist/layout.tsx
@@ -1,8 +1,4 @@
-'use client'
-import Brand from '@/components/Brand'
-import Link from 'next/link'
-import { signOut } from 'next-auth/react'
-import Button from '@/components/Button'
+import ArtistNavbar from '@/components/ArtistNavbar'
 
 export default function ArtistLayout ({
   children
@@ -11,67 +7,7 @@ export default function ArtistLayout ({
 }) {
   return (
     <>
-      <header>
-        <nav className='flex flex-row px-16 py-2 bg-[#1D1D1D] items-center justify-between sticky top-0 z-50'>
-          <ul className='flex items-center gap-4'>
-            <li>
-              <Link href='/artist'>
-                <Brand className='h-[48px] w-[182.4px]' color='#F3EFE0' />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/artist/upload'
-                className='group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out bg-[#1D1D1D] hover:brightness-90 hover:scale-105'
-              >
-                <span className='text-retro-white text-center font-bold text-[16px]'>Upload</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/artist/create-album'
-                className='group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out bg-[#1D1D1D] hover:brightness-90 hover:scale-105'
-              >
-                <span className='text-retro-white text-center font-bold text-[16px]'>Create Album</span>
-              </Link>
-            </li>
-          </ul>
-          <ul className='flex items-center gap-4'>
-            <li>
-              <Link
-                href='/artist/profile'
-                className='group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out bg-[#1D1D1D] hover:brightness-90 hover:scale-105'
-              >
-                <span className='text-retro-white text-center font-bold text-[16px]'>Profile</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/artist/profile/edit'
-                className='group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out bg-[#1D1D1D] hover:brightness-90 hover:scale-105'
-              >
-                <span className='text-retro-white text-center font-bold text-[16px]'>Edit profile</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/artist/profile/banner'
-                className='group box-border flex items-center justify-center gap-4 rounded-full px-[48px] py-[12px] transition-all duration-300 ease-in-out bg-[#1D1D1D] hover:brightness-90 hover:scale-105'
-              >
-                <span className='text-retro-white text-center font-bold text-[16px]'>Banner</span>
-              </Link>
-            </li>
-            <li>
-              <Button
-                type='white'
-                onClick={async () => await signOut()}
-              >
-                <span className='text-retro-black text-center font-bold text-[16px]'>Logout</span>
-              </Button>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <ArtistNavbar />
       {children}
     </>
   )
