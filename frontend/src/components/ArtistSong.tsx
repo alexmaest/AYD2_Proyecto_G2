816aa2d13a966199d4049b158629df1349bd0e42
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-09-01 15:22:17 -0600

    chore: created a catalog reusable component

--- frontend/src/components/ArtistSong.tsx
+++ frontend/src/components/ArtistSong.tsx
@@ -6,6 +6,7 @@ import { TbCircleFilled, TbPlayerPlayFilled } from 'react-icons/tb'
 import { Song } from '@/types/interfaces'
 import { deleteSong } from '@/lib/actions'
 import { useTransition } from 'react'
+import Link from 'next/link'
 
 interface Props {
   song: Song
@@ -22,15 +23,27 @@ function ArtistSong ({ song, artist }: Props) {
     }
   }
   return (
-    <form className='w-full flex flex-row items-center justify-between'>
+    <div className='w-full flex flex-row items-center justify-between bg-retro-black-900 p-4 rounded'>
       <div className='flex items-start gap-6'>
-        <Image
-          width={96}
-          height={96}
-          src='/images/album-cover.png'
-          alt='album cover'
-        />
-        <div className='flex flex-col h-24 justify-between'>
+        <Link
+          href={song.songUrl}
+          className='w-[80px] h-[80px] rounded-lg overflow-hidden relative group transition-all duration-300 hover:scale-105 ease-out'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Image
+            width={80}
+            height={80}
+            src='/images/album-cover.png'
+            alt='album cover'
+          />
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 invisible group-hover:visible rounded-full bg-retro-blue p-2'>
+            <TbPlayerPlayFilled
+              className=' text-retro-black text-[32px]'
+            />
+          </div>
+        </Link>
+        <div className='flex flex-col h-16 justify-between'>
           <h4 className='text-retro-white text-[18px] font-bold'>{song.name}</h4>
           <p className='text-retro-white text-[16px]'>{artist}</p>
           <div className='flex gap-2 items-center'>
@@ -41,7 +54,7 @@ function ArtistSong ({ song, artist }: Props) {
           </div>
         </div>
       </div>
-      <div className='flex items-center gap-6'>
+      <form className='flex items-center gap-6'>
         <Button
           type='primary'
         >
@@ -55,8 +68,8 @@ function ArtistSong ({ song, artist }: Props) {
             {isPending ? 'Deleting...' : 'Delete'}
           </span>
         </Button>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 }
 
