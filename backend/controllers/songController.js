15559d2c90f314e084f48c113b7a3d801a9a76e1
javier alfaro <javieralfarogt@gmail.com>
2023-09-05 10:33:45 -0600

    added log functionality to all endpoints (except register)

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -3,6 +3,7 @@ const artistModel = require('../models/artistModel');
 const songModel = require('../models/songModel');
 const multer = require('multer');
 require('dotenv').config();
+const {logEventsWrite} = require('../Helpers/logEvents');//logs
 
 class songController {
     constructor() { }
@@ -18,9 +19,12 @@ class songController {
             uploadMiddleware(req, res, async (err) => {
                 if (err) {
                     console.error(err);
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","Internal Server Error",3)//log
                     return res.status(500).send('Internal Server Error');
+                    
                 }
                 if (!req.file) {
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","No detected file in request",3)//log
                     return res.status(400).send('No detected file in request');
                 }
                 const songUrl = await userController.uploadSong(req.file.buffer)
@@ -28,6 +32,7 @@ class songController {
                     const { userId, name, duration, genre } = req.body;
                     const artistId = await artistModel.getArtistIdByUserId(userId);
                     if (!artistId) {
+                        logEventsWrite(req.originalUrl,req.method,"creador contenido","Artist account with that id doesnt exist",3)//log
                         res.status(502).send('Artist account with that id doesnt exist');
                     } else {
                         const newSong = {
@@ -39,17 +44,21 @@ class songController {
                         };
                         const savedSong = await songModel.saveSong(newSong);
                         if (savedSong) {
+                            logEventsWrite(req.originalUrl,req.method,"creador contenido","The song has been saved",3)//log
                             res.status(200).send('The song has been saved');
                         } else {
+                            logEventsWrite(req.originalUrl,req.method,"creador contenido","The song could not be saved",3)//log
                             res.status(501).send('The song could not be saved');
                         }
                     }
                 } else {
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","An error has occurred while uploading the song",3)//log
                     res.status(500).send('An error has occurred while uploading the song');
                 }
             });
         } catch (err) {
             console.error(err);
+            logEventsWrite(req.originalUrl,req.method,"creador contenido","Internal Server Error",3)//log
             res.status(500).send('Internal Server Error');
         }
     }
@@ -59,17 +68,41 @@ class songController {
             const artistId = await artistModel.getArtistIdByUserId(req.params.userId);
             if (!artistId) {
                 res.status(502).send('Artist account with that id doesnt exist');
+                if (req.originalUrl.includes("user")) {
+                    // La URL contiene el extracto "usuario"
+                    logEventsWrite(req.originalUrl,req.method,"usuario","Artist account with that id doesnt exist",3)//log
+                  }else{
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","Artist account with that id doesnt exist",3)//log
+                  }
             } else {
                 const songs = await songModel.getAllArtistSongs(artistId);
                 if (songs) {
                     res.status(200).json(songs);
+                    if (req.originalUrl.includes("user")) {
+                        // La URL contiene el extracto "usuario"
+                        logEventsWrite(req.originalUrl,req.method,"usuario","canciones de artista enviado correctamente!",3)//log
+                      }else{
+                        logEventsWrite(req.originalUrl,req.method,"creador contenido","canciones de artista enviado correctamente!",3)//log
+                      }
                 } else {
                     res.status(204).json('The songs could not be obtained');
+                    if (req.originalUrl.includes("user")) {
+                        // La URL contiene el extracto "usuario"
+                        logEventsWrite(req.originalUrl,req.method,"usuario","The songs could not be obtained",3)//log
+                      }else{
+                        logEventsWrite(req.originalUrl,req.method,"creador contenido","The songs could not be obtained",3)//log
+                      }
                 }
             }
         } catch (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
+            if (req.originalUrl.includes("user")) {
+                // La URL contiene el extracto "usuario"
+                logEventsWrite(req.originalUrl,req.method,"usuario","Internal Server Error",3)//log
+              }else{
+                logEventsWrite(req.originalUrl,req.method,"creador contenido","Internal Server Error",3)//log
+              }
         }
     }
 
@@ -78,17 +111,22 @@ class songController {
             const artistId = await artistModel.getArtistIdByUserId(req.params.userId);
             if (!artistId) {
                 res.status(502).send('Artist account with that id doesnt exist');
+                logEventsWrite(req.originalUrl,req.method,"creador contenido","Artist account with that id doesnt exist",3)//log
+                
             } else {
                 const songs = await songModel.getAllArtistAvailableSongs(artistId);
                 if (songs) {
                     res.status(200).json(songs);
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","canciones disponibles de artistas enviadas correctamente!",3)//log
                 } else {
                     res.status(501).json('The songs could not be deleted');
+                    logEventsWrite(req.originalUrl,req.method,"creador contenido","The songs could not be deleted",3)//log
                 }
             }
         } catch (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
+            logEventsWrite(req.originalUrl,req.method,"creador contenido","Internal Server Error",3)//log
         }
     }
 
@@ -96,12 +134,15 @@ class songController {
         try{
             const deleted = await songModel.deleteSong(req.params.id)
             if(deleted){
+                logEventsWrite(req.originalUrl,req.method,"creador contenido","cancion eliminada con exito!",3)//log
                 res.status(200)
+                
             }else{
 
             }            
         } catch (err) {
             console.error(err);
+            logEventsWrite(req.originalUrl,req.method,"creador contenido","Internal Server Error",3)//log
             res.status(500).send('Internal Server Error');
         }
     }
@@ -124,13 +165,16 @@ class songController {
                 const songs = await songModel.getAllArtistSongs2();
                 if (songs) {
                     res.status(200).json(songs);
+                    logEventsWrite(req.originalUrl,req.method,"usuario","canciones enviadas correctamente!",3)//log
                 } else {
                     res.status(204).json('The songs could not be obtained');
+                    logEventsWrite(req.originalUrl,req.method,"usuario","The songs could not be obtained",3)//log
                 }
             
         } catch (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
+            logEventsWrite(req.originalUrl,req.method,"usuario","Internal Server Error",3)//log
         }
     }
 
@@ -142,13 +186,16 @@ class songController {
                 const songs = await songModel.getAllAlbumSongs(req.params.idAlbum);
                 if (songs) {
                     res.status(200).json(songs);
+                    logEventsWrite(req.originalUrl,req.method,"usuario","canciones enviadas correctamente!",3)//log
                 } else {
                     res.status(204).json('The songs could not be obtained');
+                    logEventsWrite(req.originalUrl,req.method,"usuario","The songs could not be obtained",3)//log
                 }
             
         } catch (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
+            logEventsWrite(req.originalUrl,req.method,"usuario","Internal Server Error",3)//log
         }
     }
 
