8b49db8aa85abd1d22d562b677433382f85df7bb
javier alfaro <javieralfarogt@gmail.com>
2023-09-29 15:06:34 -0600

    Added filters to reports of the last phase

--- backend/routers/adminRoute.js
+++ backend/routers/adminRoute.js
@@ -8,10 +8,20 @@ router.post('/artistsManagment', adminController.artistsStatus);
 
 //top 5 songs
 router.get('/TopSongs', adminController.topSongs);
-//top 5 songs
+//top 5 albums
 router.get('/TopAlbums', adminController.topAlbums);
-//top 5 songs
+//top 5 artists
 router.get('/TopArtists', adminController.topArtists);
 
+
+//fase 3
+router.get('/allSongsGenres', adminController.allGenres);//para el top songs por filtro de generos
+
+//top 5 songs con filtro
+router.post('/TopSongsFiltro', adminController.topSongsFiltro);
+//top 5 albums
+router.post('/TopAlbumsFiltro', adminController.topAlbumsFiltro);
+//top 5 artists
+router.post('/TopArtistsFiltro', adminController.topArtistsFiltro);
 
 module.exports = router;
