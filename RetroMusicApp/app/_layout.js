82335984622d2ac8924c1674535dad0ab400da66
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-09-29 16:56:10 -0600

    chore: added landing page

--- RetroMusicApp/app/_layout.js
+++ RetroMusicApp/app/_layout.js
@@ -1,11 +1,7 @@
-import { Slot } from 'expo-router'
-import Header from '../src/components/Header'
+import { Stack } from 'expo-router'
 
 export default function HomeLayout () {
   return (
-    <>
-      <Header />
-      <Slot />
-    </>
+    <Stack />
   )
 }
