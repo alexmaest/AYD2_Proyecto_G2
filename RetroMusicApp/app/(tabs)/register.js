19cf0319327cfa5a48c32036ce67df52957e250e
Alexis Estrada <alexis1estrada@hotmail.com>
2023-10-02 17:03:41 -0600

    Add button, alert, input components
    -Added layout for button, alert and input components

--- RetroMusicApp/app/(tabs)/register.js
+++ RetroMusicApp/app/(tabs)/register.js
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Alert from '../../components/Alert'
+import CustomButton from '../../components/Button'
+
+const message = 'You have successfully registered!'
+
+const styles = StyleSheet.create({
+  primary: {
+    backgroundColor: 'bg-retro-green'
+  },
+  secondary: {
+    backgroundColor: 'bg-retro-orange'
+  },
+  black: {
+    backgroundColor: 'bg-[#1D1D1D]'
+  },
+  white: {
+    backgroundColor: 'bg-retro-white'
+  }
+})
 
 export default function Page () {
   return (
     <View className='flex-1 items-center justify-center bg-gray-900'>
-      <Text className='font-bold text-white text-2xl'>Register</Text>
+      <Alert type='success' text={message} />
+      <Text className='text-retro-white font-bold text-[25px] w-[450px] text-center'>Sign up for free to start listening.</Text>
+      <CustomButton
+        style={styles.primary} type='primary' text='Sign Up'
+      />
     </View>
   )
 }
