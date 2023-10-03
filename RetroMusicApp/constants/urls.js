caa2f1e109881095218fba838e477da0fd033289
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-10-03 12:07:18 -0600

    add login

---
+++ RetroMusicApp/constants/urls.js
@@ -0,0 +1,44 @@
+export const baseUrl = 'http://192.168.0.14:5000'
+
+export const apiUrls = {
+  auth: {
+    login: '/login',
+    register: '/artistRegister',
+    recoverPassword: '/login/passwordChange',
+    updatePassword: '/login/passwordChange/update',
+    userRegister: '/userRegister/free'
+  },
+  artist: {
+    uploadBanner: '/artist/banner',
+    banner: '/artist/banner',
+    uploadSong: '/artist/uploadSong',
+    profileConfig: '/artist/ProfileConfig',
+    profile: '/artist/Profile',
+    getSongs: '/artist/songs',
+    deleteSong: '/artist/deleteSong',
+    createAlbum: '/artist/createAlbum',
+    getAlbums: '/artist/albums',
+    deleteAlbum: '/artist/deleteAlbum',
+    getAvailableSongs: '/artist/availableSongs'
+  },
+  admin: {
+    artists: '/admin/artists',
+    artistsManagment: '/admin/artistsManagment',
+    topSongs: '/admin/TopSongs',
+    topAlbums: '/admin/TopAlbums',
+    topArtists: '/admin/TopArtists'
+  },
+  user: {
+    songs: '/user/songs',
+    albums: '/user/albums',
+    songsForAlbumId: '/user/albums/songs',
+    artists: '/user/artists',
+    artistSongs: '/user/artists/songs',
+    artistAlbums: '/user/artists/albums',
+    profile: '/user/Profile',
+    profileConfig: '/user/ProfileConfig',
+    recomendations: '/user/recomendations',
+    musicCounter: '/user/MusicCounter',
+    userLimit: '/user/userLimit'
+  }
+}
