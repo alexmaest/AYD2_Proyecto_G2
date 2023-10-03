49459e8f433d5ccc91208b48df13d35b409049be
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-10-03 11:23:09 -0600

    chore: static top 5 songs report

---
+++ RetroMusicApp/app/admin/_layout.js
@@ -0,0 +1,59 @@
+import { Tabs } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
+import { View, StyleSheet } from 'react-native'
+
+const AdminLayout = () => {
+  return (
+    <Tabs
+      initialRouteName='home'
+      screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { backgroundColor: '#1D1D1D' } }}
+    >
+      <Tabs.Screen
+        name='home'
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.IconView}>
+              <TabBarIcon name='home' size={24} focused={focused} />
+            </View>
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='reports'
+        options={{
+          title: '',
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.IconView}>
+              <TabBarIcon name='signal' size={24} focused={focused} />
+            </View>
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='(list)/top-five-songs'
+        options={{ href: null }}
+      />
+    </Tabs>
+  )
+}
+
+function TabBarIcon (props) {
+  return (
+    <FontAwesome
+      size={props.size || 26}
+      style={{ color: props.focused ? '#1D5B79' : '#F3EFE0' }}
+      {...props}
+    />
+  )
+}
+
+export default AdminLayout
+
+const styles = StyleSheet.create({
+  IconView: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  }
+})
