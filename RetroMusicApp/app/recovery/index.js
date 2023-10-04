75094088b19a4c1d64b30d8a032afcdd47e97c4e
javier alfaro <javieralfarogt@gmail.com>
2023-10-03 18:57:30 -0600

    added alerts in recovery password

--- RetroMusicApp/app/recovery/index.js
+++ RetroMusicApp/app/recovery/index.js
@@ -5,18 +5,29 @@ import {
   TextInput,
   TouchableOpacity,
   ImageBackground,
-  SafeAreaView
+  SafeAreaView,
+  // Button,
+  Alert
 } from 'react-native'
 
 import { Stack, Link } from 'expo-router'
 import HeaderButton from '../../components/HeaderButton'
 import { ScrollView } from 'react-native-gesture-handler'
+// import { useNavigation } from '@react-navigation/native'
 
 const RecoveryPassword = () => {
   const [email, setEmail] = useState('')
   const [code, setCode] = useState('')
   const [password, setPassword] = useState('')
   const [codeVisible, setCodeVisible] = useState(false)
+  const [alert, setAlert] = useState(false)
+  const [mensaje0, setMensaje0] = useState('')
+  const [mensaje, setMensaje] = useState('')
+  // const navigate = useNavigation()
+
+  // ojo porque cada vez que encienda la EC2 cambia la IP xc
+
+  const EC2 = '18.223.44.184'
 
   const handleEmailChange = (text) => {
     setEmail(text)
@@ -31,7 +42,6 @@ const RecoveryPassword = () => {
   }
 
   async function enviarCorreo (text) {
-    setCodeVisible(true)// para que se mire el resto
     console.log('\n\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nEnvio de codigo(solicitud de cambio de password) a correo electronico')
     // console.log(' >>>> recibo:' + text)
     const email = { // newPassword, token
@@ -41,7 +51,7 @@ const RecoveryPassword = () => {
     console.log(JSON.stringify(email))
 
     try {
-      const response = await fetch('http://18.222.196.155:5000/login/passwordChange', {
+      const response = await fetch('http://' + EC2 + ':5000/login/passwordChange', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -49,19 +59,36 @@ const RecoveryPassword = () => {
         body: JSON.stringify(email)
       })
 
+      var validation = false
+
       if (response && response.status !== 200) {
         if (response.status === 406) {
           console.log('This email address currently has a valid token. Please check your email.')
+          setMensaje0('Esta direccion de correo, ya posee un token valido, porfavor revise su bandeja de correo electronico!')
+          Alert.alert('Alerta', 'Esta direccion de correo, ya posee un token valido, porfavor revise su bandeja de correo electronico!', [{ text: 'Aceptar', onPress: () => console.log('alert closed') }])
+          setCodeVisible(true)// para que se mire el resto
+          return
         } else {
+          setMensaje0('No pudimos hallar una cuenta con el correo brindado')
+          Alert.alert('Error', 'No pudimos hallar una cuenta con el correo brindado', [{ text: 'Aceptar', onPress: () => console.log('alert closed') }])
+          validation = true
           throw new Error("We couldn't find an account with that email address.")
         }
       }
+      setMensaje0('revise el msg enviado a su correo electronico')
+      Alert.alert('Enviado', 'revise el msg enviado a su correo electronico', [{ text: 'Aceptar', onPress: () => console.log('alert closed') }])
     } catch (error) {
       console.log(error)
+    }
+
+    // si no arroja trow porq no existe el correo pues mostramos el siguiente paso
+    if (!validation) {
+      setCodeVisible(true)// para que se mire el resto
     }
   }
 
   async function nuevaContraseña (password, codigo) {
+    setAlert(true)
     setCodeVisible(true)// para que se mire el resto
     console.log('\n\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nSolicitud de cambio de password')
     // console.log(' >>>> recibo:' + text)
@@ -73,7 +100,7 @@ const RecoveryPassword = () => {
     console.log(JSON.stringify(body))
 
     try {
-      const response = await fetch('http://18.222.196.155:5000/login/passwordChange/update', {
+      const response = await fetch('http://' + EC2 + ':5000/login/passwordChange/update', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -82,10 +109,14 @@ const RecoveryPassword = () => {
       })
 
       if (response.status !== 200) {
+        setMensaje('no pudimos realizar el cambio de tu contraseña, porfavor revisa si el token ingresado es el correcto!')
+        Alert.alert('Error', 'no pudimos realizar el cambio de tu contraseña, porfavor revisa si el token ingresado es el correcto!', [{ text: 'Aceptar', onPress: () => console.log('alert closed') }])
         throw new Error("We couldn't update your password.")
       }
 
       console.log('todo salio bien!!!')
+      setMensaje('cambio de contraseña realizado con exito!')
+      Alert.alert('Cambio realizado', 'cambio de contraseña realizado con exito!', [{ text: 'Aceptar', onPress: () => console.log('alert closed') }])
     } catch (error) {
       console.log(error)
     }
@@ -116,6 +147,7 @@ const RecoveryPassword = () => {
             headerShadowVisible: false,
             headerTitle: ''
           }}
+
         />
         <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexGrow: 1 }}>
           <View className='flex-1 items-center justify-center max-w-full'>
@@ -148,10 +180,10 @@ const RecoveryPassword = () => {
 
                 </View>
                 <View style={{ padding: 16, display: codeVisible ? 'flex' : 'none' }}>
-                  <Text className='text-retro-blue text-md font-bold'>revise el msg enviado a su correo electronico</Text>
+                  <Text className='text-retro-blue text-md font-bold'>{mensaje0}</Text>
                 </View>
                 <View style={{ padding: 16, borderTopColor: '#000000', borderTopWidth: 1, display: codeVisible ? 'flex' : 'none' }}>
-                  <Text className='text-retro-white text-lg font-bold'>Ingresa el codigo que te enviamos</Text>
+                  <Text className='text-retro-white text-lg font-bold'>Ingresa el codigo que te enviamos (token)</Text>
                   <TextInput
                     style={{ backgroundColor: '#1D1D1D', borderColor: 'gray', borderWidth: 1 }}
                     placeholderTextColor='#FFFFFF'
@@ -187,6 +219,9 @@ const RecoveryPassword = () => {
                     <Text style={{ color: '#fff' }}>Restablecer contraseña</Text>
                   </TouchableOpacity>
                 </View>
+                <View style={{ padding: 16, display: alert ? 'flex' : 'none' }}>
+                  <Text className='text-retro-blue text-md font-bold'>{mensaje}</Text>
+                </View>
               </View>
             </View>
 
@@ -198,7 +233,7 @@ const RecoveryPassword = () => {
             }}
           >
             <View>
-              <Link href='/login' className='underline text-retro-black'>
+              <Link href='/login' className='underline text-retro-black' style={{ fontSize: 20, fontWeight: 'bold' }}>
                 Login
               </Link>
             </View>
