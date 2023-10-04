fb0b2f404c1b91f8ed8843665ffbdaf0c68b4693
Alexis Estrada <alexis1estrada@hotmail.com>
2023-10-03 18:05:04 -0600

    Add user and artist register view
    -Added user register app view
    -Added artist register app view

--- RetroMusicApp/components/Alert.jsx
+++ RetroMusicApp/components/Alert.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const alertStyles = {
-  danger: { backgroundColor: 'red', borderColor: 'red', color: 'red' },
-  success: { backgroundColor: 'lightgreen', borderColor: 'green', color: 'green' },
-  warning: { backgroundColor: 'yellow', borderColor: 'yellow', color: 'yellow' }
+  danger: { backgroundColor: '#f53636', borderColor: '#f53636', color: '#f53636' },
+  success: { backgroundColor: '#36f576', borderColor: '#36f576', color: '#36f576' },
+  warning: { backgroundColor: '#fcff4a', borderColor: '#fcff4a', color: '#fcff4a' }
 }
 
 const styles = StyleSheet.create({
@@ -14,20 +14,29 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 8,
     flexDirection: 'row'
+  },
+  closeButton: {
+    padding: 8
   }
 })
 
 const Alert = ({ type, text }) => {
-  return (
-    <View
-      style={[
-        styles.alertContainer,
-        alertStyles[type]
-      ]}
-    >
-      <Text className='font-bold text-white text-2xl'>{text}</Text>
-    </View>
-  )
+  const [isVisible, setIsVisible] = useState(true)
+
+  const closeAlert = () => {
+    setIsVisible(false)
+  }
+
+  return isVisible
+    ? (
+      <View style={[styles.alertContainer, alertStyles[type]]}>
+        <Text className='font-bold text-white text-2xl'>{text}</Text>
+        <TouchableOpacity onPress={closeAlert} style={styles.closeButton}>
+          <Text style={{ color: 'black', fontSize: 15 }}>x</Text>
+        </TouchableOpacity>
+      </View>
+      )
+    : null
 }
 
 export default Alert
---
+++ RetroMusicApp/constants/urls.js
@@ -0,0 +1,17 @@
+export const baseUrl = 'http://192.168.1.19:5000'
+
+export const apiUrls = {
+  auth: {
+    login: '/login',
+    register: '/artistRegister',
+    recoverPassword: '/login/passwordChange',
+    updatePassword: '/login/passwordChange/update',
+    userRegister: '/userRegister/free'
+  },
+  artist: {
+  },
+  admin: {
+  },
+  user: {
+  }
+}
