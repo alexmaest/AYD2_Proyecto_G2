4061832f1d4edc9d89cf3cdc16d32a60bca6221a
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-10-05 17:45:02 -0600

    chore: added top global songs report with filter

--- RetroMusicApp/app/admin/(list)/top-global-songs.js
+++ RetroMusicApp/app/admin/(list)/top-global-songs.js
@@ -1,69 +1,113 @@
 import { Dimensions, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { useEffect, useState } from 'react'
-// import RetroButton from '../../../components/RetroButton'
+import { Picker } from '@react-native-picker/picker'
+import RetroButton from '../../../components/RetroButton'
 
-const url = 'http://localhost:5000/admin/TopSongs'
+const url = 'http://localhost:5000/admin/TopSongsAlltime'
+const url2 = 'http://localhost:5000/admin/allSongsDates'
 
 const TopGlobalSongs = () => {
   const [labels, setLabels] = useState([])
   const [data, setData] = useState([])
-  // const [startDate, setStartDate] = useState(new Date())
-  // const [endDate, setEndDate] = useState(new Date())
+  const [dates, setDates] = useState([])
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
 
   useEffect(() => {
-    const fetchSongs = async () => {
+    const fetchDates = async () => {
       try {
-        const res = await fetch(url, {
+        const res = await fetch(url2, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
           }
         })
         const data = await res.json()
-        setLabels(data.map((song) => song.name))
-        setData(data.map((song) => song.plays))
+        setStartDate(data[0])
+        setEndDate(data[data.length - 1])
+        setDates(data)
       } catch (error) {
-        setLabels([])
-        setData([])
+        setStartDate('')
+        setEndDate('')
+        setDates([])
       }
     }
-    fetchSongs()
+    fetchDates()
   }, [])
 
-  // const filterSongs = async () => {
-  //   try {
-  //     const res = await fetch('http://localhost:5000/admin/TopSongsFiltro', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ fechaInicio: startDate, fechaFin: endDate })
-  //     })
-  //     const data = await res.json()
-  //     setLabels(data.map((song) => song.name))
-  //     setData(data.map((song) => song.plays))
-  //   } catch (error) {
-  //     setLabels([])
-  //     setData([])
-  //   }
-  // }
+  const handleFilter = async () => {
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ dateInit: startDate, dateFinal: endDate })
+      })
+      const data = await res.json()
+      setLabels(data.map((song) => song.cancion))
+      setData(data.map((song) => song.artista))
+    } catch (error) {
+      setLabels([])
+      setData([])
+    }
+  }
 
   return (
     <SafeAreaView style={styles.Container}>
-      <Text style={styles.Text}>Top Global Songs</Text>
-      {/* <View style={{ flexDirection: 'column', justifyContent: 'space-between', width: '100%' }}>
-        <RetroButton type='white' text='Filter' handlePress={async () => { await filterSongs() }} />
-      </View> */}
+      <Text style={styles.Text}>Top Global de Canciones</Text>
+      <View style={{ flexDirection: 'column', justifyContent: 'space-between', width: '100%', gap: 16 }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+            gap: 16
+          }}
+        >
+          <Picker
+            style={{
+              backgroundColor: '#F3EFE0',
+              borderWidth: 1,
+              borderRadius: 5,
+              padding: 10,
+              color: '#222222',
+              width: '45%'
+            }}
+            selectedValue={startDate}
+            onValueChange={(itemValue) => setStartDate(itemValue)}
+          >
+            {dates.map((date, index) => (
+              <Picker.Item key={index} label={date} value={date} />
+            ))}
+          </Picker>
+          <Picker
+            style={{
+              backgroundColor: '#F3EFE0',
+              borderWidth: 1,
+              borderRadius: 5,
+              padding: 10,
+              color: '#222222',
+              width: '45%'
+            }}
+            selectedValue={endDate}
+            onValueChange={(itemValue) => setEndDate(itemValue)}
+          >
+            {dates.map((date, index) => (
+              <Picker.Item key={index} label={date} value={date} />
+            ))}
+          </Picker>
+        </View>
+        <RetroButton type='white' text='Mostrar' handlePress={handleFilter} />
+      </View>
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.Table}>
         <View style={styles.TableRow}>
-          <Text style={styles.TableHeader}>#</Text>
-          <Text style={styles.TableHeader}>Song</Text>
-          <Text style={styles.TableHeader}>Plays</Text>
+          <Text style={styles.TableHeader}>Canción</Text>
+          <Text style={styles.TableHeader}>Artista</Text>
         </View>
         {labels.map((label, index) => (
           <View key={index} style={styles.TableRow}>
-            <Text style={styles.TableCell}>{index + 1}</Text>
             <Text style={styles.TableCell}>{label}</Text>
             <Text style={styles.TableCell}>{data[index]}</Text>
           </View>
