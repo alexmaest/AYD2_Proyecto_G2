4338b9bb3d4d7030097464724548b825cd642418
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-10-18 20:20:58 -0600

    add player

--- RetroMusicApp/app/user/_layout.js
+++ RetroMusicApp/app/user/_layout.js
@@ -5,83 +5,86 @@ import MusicPlayer from '../../components/Player'
 
 const UserLayout = () => {
   return (
-    <Tabs
-      initialRouteName='home'
-      screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { backgroundColor: '#1D1D1D' } }}
-    >
-      <Tabs.Screen
-        name='home'
-        options={{
-          title: '',
-          tabBarIcon: ({ focused }) => (
-            <View style={styles.IconView}>
-              <TabBarIcon name='home' size={24} focused={focused} />
-            </View>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name='reports'
-        options={{
-          title: '',
-          tabBarIcon: ({ focused }) => (
-            <View style={styles.IconView}>
-              <TabBarIcon name='signal' size={24} focused={focused} />
-            </View>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name='discover/index'
-        options={{
-          title: '',
-          tabBarIcon: ({ focused }) => (
-            <View style={styles.IconView}>
-              <TabBarIcon name='search' size={24} focused={focused} />
-            </View>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name='discover/genre'
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name='album'
-        options={{
-          tabBarButton: (props) => null
-        }}
-      />
-      <Tabs.Screen
-        name='artist'
-        options={{
-          tabBarButton: (props) => null
-        }}
-      />
-      <Tabs.Screen
-        name='(list)/songs-listened'
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name='(list)/number-songs'
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name='(list)/listened-time'
-        options={{ href: null }}
-      />
-      <Tabs.Screen
-        name='info'
-        options={{
-          title: '',
-          tabBarIcon: ({ focused }) => (
-            <View style={styles.IconView}>
-              <TabBarIcon name='user' size={24} focused={focused} />
-            </View>
-          )
-        }}
-      />
-    </Tabs>
+    <>
+      <Tabs
+        initialRouteName='home'
+        screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { backgroundColor: '#1D1D1D' } }}
+      >
+        <Tabs.Screen
+          name='home'
+          options={{
+            title: '',
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.IconView}>
+                <TabBarIcon name='home' size={24} focused={focused} />
+              </View>
+            )
+          }}
+        />
+        <Tabs.Screen
+          name='reports'
+          options={{
+            title: '',
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.IconView}>
+                <TabBarIcon name='signal' size={24} focused={focused} />
+              </View>
+            )
+          }}
+        />
+        <Tabs.Screen
+          name='discover/index'
+          options={{
+            title: '',
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.IconView}>
+                <TabBarIcon name='search' size={24} focused={focused} />
+              </View>
+            )
+          }}
+        />
+        <Tabs.Screen
+          name='discover/genre'
+          options={{ href: null }}
+        />
+        <Tabs.Screen
+          name='album'
+          options={{
+            tabBarButton: (props) => null
+          }}
+        />
+        <Tabs.Screen
+          name='artist'
+          options={{
+            tabBarButton: (props) => null
+          }}
+        />
+        <Tabs.Screen
+          name='(list)/songs-listened'
+          options={{ href: null }}
+        />
+        <Tabs.Screen
+          name='(list)/number-songs'
+          options={{ href: null }}
+        />
+        <Tabs.Screen
+          name='(list)/listened-time'
+          options={{ href: null }}
+        />
+        <Tabs.Screen
+          name='info'
+          options={{
+            title: '',
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.IconView}>
+                <TabBarIcon name='user' size={24} focused={focused} />
+              </View>
+            )
+          }}
+        />
+      </Tabs>
+      <MusicPlayer />
+    </>
   )
 }
 
